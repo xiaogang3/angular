bd8cd2cb917dcb8176836b4f82996ae2d004fac0
xiaogan <mr_w0922@163.com>
2025-06-01 19:08:08 +0800

    angular1.0

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+
+
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
